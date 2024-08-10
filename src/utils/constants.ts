20d2f12552da10328d7bd461507fcf9b21059d57
sanjay2022data <tiwarysa@amazon.com>
2024-08-10 11:39:31 +0530

    added

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const API_NAME = "api";
+export const MODEL_MAX_INPUT_LENGTH = 375000;
+
+export const MIN_PROMPT_TEMPLATE_LENGTH = 0;
+export const MAX_PROMPT_TEMPLATE_LENGTH = 375000;
+export const END_CONVERSATION_TOKEN = "##END_CONVERSATION##";
+
+export const INTERNAL_USER_GENAI_POLICY_URL =
+  "https://policy.a2z.com/docs/568686/publication";
+
+
+export const SOURCE_DOCS_RESPONSE_PAYLOAD_KEY = "sourceDocument";
+
+
+export const SOCKETURL =
+  "wss://t5r0xjryuc.execute-api.ap-south-1.amazonaws.com";
+
+export const USERPOOLID = "ap-south-1_qYAh4crvz";
+export const CLIENTID = "668qjbjlqtan4ugcjn4hs7tt08";
+
+export const USERNAME = "tiwarysa-admin";
+export const PASSWORD = "Julsanjay@0721";
